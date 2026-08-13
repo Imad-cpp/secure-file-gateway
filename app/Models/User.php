3c319f64c5a9ee951e7dcd90f1b1afcff2146813
Imadeddine Es-sebaiy <Imadeddine.essebaiy@gmail.com>
2026-08-13 02:05:46 +0100

    fix: type stored file relationship

--- app/Models/User.php
+++ app/Models/User.php
@@ -31,6 +31,9 @@ class User extends Authenticatable
         ];
     }
 
+    /**
+     * @return HasMany<StoredFile, $this>
+     */
     public function storedFiles(): HasMany
     {
         return $this->hasMany(StoredFile::class, 'owner_id');
